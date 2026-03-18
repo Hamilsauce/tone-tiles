e19c7a49bdb50a2f1720f448e88230b8d14af98c
hamilsauce <jake.w.hamilton@outlook.com>
2026-03-17 22:26:20 -0500

    suck

--- ui/SVGCanvasHost.js
+++ ui/SVGCanvasHost.js
@@ -1,10 +1,10 @@
-import { ref, computed, watch, onMounted } from 'vue'
+import { ref, computed, toValue, onMounted } from 'vue'
 import { defineComponent, getTemplate } from '../lib/vue-helpers.js';
 import { useAppState } from '../store/app.store.js';
 import { useMapStore } from '../store/map.store.js';
 import { runCanvas } from '../run-canvas.js';
 import { AudioClockLoop } from '../lib/loop-engine.js'
-import { router, RouteName } from '../router/router.js'
+import { router, route } from '../router/router.js'
 
 export const SVGCanvasHost = defineComponent(
   getTemplate('svg-canvas-host'),
@@ -12,7 +12,7 @@ export const SVGCanvasHost = defineComponent(
     const { mapState } = useMapStore();
     const { isRunning, setRunning } = useAppState();
     const map = computed(() => mapState.value);
-    const mapId = computed(() => router.params.id);
+    const mapId = computed(() => toValue(route.value.params.id));
     let canvasEl; // = document.querySelector('#canvas');
     let viewport; // = document.querySelector('#canvas');
     let scene; // = canvasEl.querySelector('#scene');
@@ -23,35 +23,34 @@ export const SVGCanvasHost = defineComponent(
     const routine1 = (dt, now) =>{
       console.warn('routine 1', dt, now)
     }
- 
+
     const render = (dt, now) =>{
       console.warn('render', dt, now)
     }
- 
+
     const loopEngine = new AudioClockLoop({
       routines:[routine1],
       render,
     })
     // loopEngine.start()
-    
+
     const handleRunningToggle = () => {
       isRunning.value = !isRunning.value;
     }
-    
+
     const centerViewport = () => {
       viewport.setAttribute('transform', 'matrix(1 0 0 1 0 0)')
     }
-    
+
     onMounted(() => {
       canvasEl = document.querySelector('#canvas');
       viewport = canvasEl.querySelector('#viewport');
       scene = canvasEl.querySelector('#scene');
       tileLayer = scene.querySelector('#tile-layer');
       objectLayer = scene.querySelector('#object-layer');
-      
       runCanvas(mapId.value);
     });
-    
+
     return { centerViewport, map }
   }, {
     components: {}
--- ui/map-selection.js
+++ ui/map-selection.js
@@ -1,4 +1,4 @@
-import { ref, computed, watch } from 'vue';
+import { ref, computed, watch, toValue } from 'vue';
 import { getLinkCoords, getDirectionFromPoints } from '../lib/graph.model.js';
 
 import { BLANK_MAP_16X16, mapStorageFormatter } from '../maps.js';
@@ -17,34 +17,37 @@ let hasInitViewBox = false;
 let mapStore = useMapStore();
 
 const renderMap = (mapData, svgCanvas, graph, actor1, selectionBox) => {
+  mapData = toValue(mapData);
+  // console.warn({ mapData });
+
   graph.fromMap(mapData);
-  
+
   selectionBox.setBounds({
     minX: 0,
     minY: 0,
     maxX: graph.width,
     maxY: graph.height
   });
-  
+
   hasInitViewBox = true;
-  
+
   svgCanvas.layers.tile.innerHTML = '';
-  
-  const tilesTotal = graph.width * graph.height
-  console.log({ tilesTotal })
+
+  const tilesTotal = graph.width * graph.height;
+  console.log({ tilesTotal });
   if (tilesTotal > 512) {
-    svgCanvas.layers.tile.classList.add('no-shadow')
+    svgCanvas.layers.tile.classList.add('no-shadow');
   } else {
-    svgCanvas.layers.tile.classList.remove('no-shadow')
+    svgCanvas.layers.tile.classList.remove('no-shadow');
   }
-  
+
   svgCanvas.layers.tile.innerHTML = '';
-  
+
   graph.nodes.forEach(({ x, y, tileType }, rowNumber) => {
     if (tileType === 'start') {
       actor1.setAttribute('transform', `translate(${x},${y})`);
     }
-    
+
     svgCanvas.layers.tile.append(
       svgCanvas.createTileObject({
         tileType: mapStore.previousMapId.value && tileType === 'start' ? 'empty' : tileType,
@@ -55,12 +58,10 @@ const renderMap = (mapData, svgCanvas, graph, actor1, selectionBox) => {
         isPathNode: false,
       }).show().dom);
   });
-  
-  
-  
+
   Object.entries((mapData.linkedMaps)).forEach(([dir, linkedMap], i) => {
-    const { x, y } = getLinkCoords(dir, { width: graph.width, height: graph.height })
-    
+    const { x, y } = getLinkCoords(dir, { width: graph.width, height: graph.height });
+
     svgCanvas.layers.tile.append(
       svgCanvas.createTile({
         linkedMap,
@@ -71,61 +72,61 @@ const renderMap = (mapData, svgCanvas, graph, actor1, selectionBox) => {
         active: false,
         isPathNode: false,
       }));
-    
+
     if (mapStore.previousMapId.value === linkedMap) {
       actor1.setAttribute('transform', `translate(${x},${y})`);
     }
-  })
-  
-  svgCanvas.layers.surface.setAttribute('transform', `translate(${Math.floor((graph.width+2)/2)-0.3}, ${Math.floor((graph.height + 2) / 2) - 0.25})`)
-  svgCanvas.layers.surface.querySelector('#surface-map-name').setAttribute('transform', `translate(0, ${-((graph.height/2))-3}) scale(0.4)`)
+  });
+
+  svgCanvas.layers.surface.setAttribute('transform', `translate(${Math.floor((graph.width + 2) / 2) - 0.3}, ${Math.floor((graph.height + 2) / 2) - 0.25})`);
+  svgCanvas.layers.surface.querySelector('#surface-map-name').setAttribute('transform', `translate(0, ${-((graph.height / 2)) - 3}) scale(0.4)`);
 };
 
 
 export const initMapControls = async (graph, svgCanvas, actor1, selectionBox) => {
   hasInitViewBox = false;
   mapStore = useMapStore();
-  
+
   const app = document.querySelector('#app');
   const appBody = document.querySelector('#app-body');
   const containers = document.querySelectorAll('.container');
-  
+
   const saveButton = document.querySelector('#save-map');
   const newButton = document.querySelector('#new-map');
-  
+
   const mapNames = await loadMapIndex();
-  
-  
+
+
   const blankOpt = { id: null, name: '' };
   const defaultOpt = { id: '', name: '' };
-  
+
   saveButton.addEventListener('click', async (e) => {
     e.preventDefault();
     e.stopPropagation();
     e.stopImmediatePropagation();
-    
+
     let mapId;
-    
+
     const graphOut = graph.toStorageFormat();
-    
+
     if (!mapStore.isMapSaved.value) {
       delete graphOut.id;
     }
-    
+
     mapId = await storeMap({ ...graphOut, ...(mapStore.currentMapIndex.value || {}) });
-    
+
     copyTextToClipboard(graphOut);
-    
+
     return mapId;
   });
-  
+
   watch(mapStore.currentMap, (newMap, oldMap) => {
-    if (newMap && oldMap && newMap.id === oldMap.id) return;
-    renderMap(newMap, svgCanvas, graph, actor1, selectionBox);
+    if (!newMap.id || newMap && oldMap && newMap.id === oldMap.id) return;
+    renderMap(mapStore.currentMap, svgCanvas, graph, actor1, selectionBox);
   }, { immediate: true });
-  
-  
+
+
   return (id) => {
     mapStore.setCurrentMapById(id);
-  }
+  };
 };
