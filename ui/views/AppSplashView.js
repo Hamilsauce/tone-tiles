0c2da23899bf071a4b3a8812664455d2d9950968
hamilsauce <jacob.w.hamilton@outlook.com>
2026-03-25 22:46:44 -0500

    update

--- ui/views/AppSplashView.js
+++ ui/views/AppSplashView.js
@@ -1,15 +1,15 @@
 import { ref, computed, watch, onMounted } from 'vue'
 import { defineComponent, getTemplate } from '../../lib/vue-helpers.js';
 import { router, route, RouteName } from '../../router/router.js'
-// import { useMapStore } from '../../store/map.store.js';
 
 const PINNED_MAP_ID = 'AV1lKxUDArOcODrZAgaQ'; // diagnal
 
 export const AppSplashView = defineComponent(
   getTemplate('app-splash-view'),
   (props, ctx) => {
-    // const mapStore = useMapStore();
+    const foregroundRef = ref(null);
     const fade = ref(false);
+    let parent;
     
     const handleClick = () => {
       fade.value = true;
@@ -21,14 +21,24 @@ export const AppSplashView = defineComponent(
       }, 2000);
     };
     
-    // watch(currentMap, () => {
-    //   console.warn(mapStats.value)
-    // }, { immediate: true })
-    
+    onMounted(() => {
+      parent = foregroundRef.value.parentElement;
+      
+      foregroundRef.value.style.opacity = 0;
+      foregroundRef.value.remove();
+      
+      setTimeout(() => {
+        parent.append(foregroundRef.value);
+        setTimeout(() => {
+          foregroundRef.value.style.opacity = 1;
+        }, 1);
+      }, 0);
+    })
     
     return {
       fade,
-      handleClick
+      handleClick,
+      foregroundRef
     }
   }, {},
 )
