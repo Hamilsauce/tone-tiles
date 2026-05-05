7835aad0454e4d4611c75e9ec03e88f238af6b7b
hamilsauce <jacob.w.hamilton@outlook.com>
2026-05-05 00:29:15 -0500

    update

--- model/Actor.model.js
+++ model/Actor.model.js
@@ -1,7 +1,12 @@
 import { TraverserModel } from './Traverser.model.js';
+import { getDirectionFromPoints } from '../core/spatial/utils.js';
+
 import { Point } from '../core/spatial/Point.js';
 import ham from 'ham';
 const { sleep } = ham;
+
+const clamp = (v, min, max) => Math.min(max, Math.max(min, v));
+
 import {
   ActorError,
   ActorGoal,
@@ -21,6 +26,7 @@ const DefaultActorProperties = {
   goalPoint: null,
 };
 
+
 export class ActorModel extends TraverserModel {
   constructor({ properties, ...rest } = {}) {
     super({
@@ -31,12 +37,29 @@ export class ActorModel extends TraverserModel {
         ...(properties ?? {}),
       },
     });
+    
+    this.prevDir = null
+    this.currDir = null
   }
   
   onGoal() {
     console.warn('actor goal reached', { id: this.id, point: this.point, goalPoint: this.goalPoint });
   }
+  
+  onMove(event) {
+    this.prevDir = this.currDir
+    this.currDir = getDirectionFromPoints(event.prevPoint, event.point)
+
+    if (this.prevDir === this.currDir) {
+      this.stepInterval = clamp(this.stepInterval - 0.008, 0.05, 0.1)
+    } else {
+      this.stepInterval = clamp(this.stepInterval + 0.012, 0.05, 0.1)
+    }
+  }
+  
   emitActorTravel(payload = {}) {
+    
+    
     return this.#emitActorAction(ActorTravel, {
       ...payload,
       point: Point.from(payload.point ?? this.point),
@@ -45,6 +68,7 @@ export class ActorModel extends TraverserModel {
   }
   
   emitActorMove(payload = {}) {
+    
     return this.#emitActorAction(ActorMove, {
       ...payload,
       point: Point.from(payload.point ?? this.point),
