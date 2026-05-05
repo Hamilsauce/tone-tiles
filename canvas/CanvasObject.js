6430ea30924c10170cf16ea16b26099aa0e1ca23
jake hamilton <jake.hamilton@zoro.com>
2026-05-04 20:40:15 -0500

    codex resolver fix1

--- canvas/CanvasObject.js
+++ canvas/CanvasObject.js
@@ -47,16 +47,16 @@ export class CanvasObject extends EventEmitter {
   #isBusy = false;
   #updateQueue = []
   #position = []
-  
+
   constructor(context = new SVGCanvas(), type = '', options) {
     super();
     const { id, model = {} } = options;
-    
+
     this.#context = context;
     this.#type = type;
-    
+
     this.#id = id ?? `${type}${utils.uuid()}`;
-    
+
     // this.#self = this.#context.useTemplate(type, {
     //   withHarness: type !== 'tile',
     //   id: this.#id,
@@ -69,220 +69,219 @@ export class CanvasObject extends EventEmitter {
       transforms: options.transforms
     });
     this.#self = template;
-    
+
     // const translate = transforms.filter(_ => _.type === 'translate')
     // const others = transforms.filter(_ => _.type !== 'translate')
     // console.warn('template, transforms', template, transforms)
     this.#position = transforms.position;
     this.#transformList = transforms.transformList;
     Object.assign(this.#model, this.#normalizeSpatialPatch(model));
-    
-    
+
+
     // this.#transformList = new TransformList(this.#context, this.#self, transforms);
     // this.#position = new TransformList(this.#context, this.#self, translate);
     // this.#transformList = new TransformList(this.#context, this.#self, transforms);
   }
-  
+
   get context() { return this.#context; }
-  
+
   get dom() { return this.#self; }
-  
+
   get model() { return this.#model; }
-  
+
   get x() { return this.#model.point?.x ?? this.#model.x; }
-  
+
   set x(v) {
     this.#model.x = v;
     this.#model.point = new CanvasPoint(v, this.y ?? 0);
   }
-  
+
   get y() { return this.#model.point?.y ?? this.#model.y; }
-  
+
   set y(v) {
     this.#model.y = v;
     this.#model.point = new CanvasPoint(this.x ?? 0, v);
   }
-  
+
   get data() { return this.dom.dataset; }
-  
+
   get isVisible() { return !this.#hide; }
-  
+
   get transforms() { return this.#transformList; }
-  
+
   get type() { return this.#type; }
-  
+
   get id() { return this.#id; }
-  
+
   get isBusy() { return this.#isBusy; }
-  
+
   set isBusy(v) {
     if (this.#isBusy && !v && this.#updateQueue.length) {
       this.#isBusy = v;
-      
-      const update = this.#updateQueue.pop()
+
+      const update = this.#updateQueue.shift()
       update();
     } else {
       this.#isBusy = v;
     }
   }
-  
+
   get point() { return this.#model.point; }
-  
+
   get layer() { return this.dom.closest('g.layer'); }
-  
+
   get parent() { return this.dom.parentElement; }
-  
-  // get isLoaded() { return !!this.parent; }
+
   get isLoaded() { return this.dom.style.display !== 'none'; }
-  
+
   subscribe(eventType, cb) {
     this.#subscriptions.set(eventType, this.on(eventType, cb));
     return this;
   }
-  
+
   unsubscribe(eventType) {
     if (!this.#subscriptions.has(eventType)) return;
     this.#subscriptions.get(eventType)();
-    
+
     return this;
   }
-  
+
   destroy() {
     this.#subscriptions.forEach(unsubFn => {
       unsubFn();
     });
-    
+
     this.remove();
-    
+
     return this;
   }
-  
+
   domPoint(x, y) {
     return new DOMPoint(x, y).matrixTransform(
       this.dom.getScreenCTM().inverse()
     );
   }
-  
+
   translateTo(x, y) {
     this.#position.translateTo(x, y);
     return this;
   }
-  
+
   rotateTo(angle, x = 0, y = 0) {
     this.transforms.rotateTo(angle, x, y);
     return this;
   }
-  
+
   scaleTo(x, y) {
     this.transforms.scaleTo(x, y);
     return this;
   }
-  
+
   remove() {
     this.dom.remove();
-    
+
     return this;
   }
-  
+
   show() {
     this.#hide = false;
     this.render({ hide: this.#hide });
     return this;
   }
-  
+
   hide() {
     this.#hide = true;
     this.render({ hide: this.#hide });
     return this;
   }
-  
+
   update(attributeMap = {}) {
     const normalizedPatch = this.#normalizeSpatialPatch(attributeMap);
-    
+
     for (const key in normalizedPatch) {
       const v = normalizedPatch[key];
       const modelV = this.#model[key];
       const hasChanged = v !== modelV;
       const isValid = !(v === undefined || modelV === undefined);
-      
+
       if (v !== undefined && hasChanged) {
         this.#model[key] = v;
       } else if (!isValid) {
         console.error(`[${this.constructor.name} ${this.id}] invalid Model patch: ${key}: ${v}\n\nReasons: isValid: ${isValid}, hasChanged: ${hasChanged}`);
       }
     }
-    
+
     this.render();
-    
+
     return this;
   }
-  
+
   render() {
     this.translateTo(this.x, this.y);
     const { point, ...model } = this.model;
     Object.assign(this.data, model, { x: this.x, y: this.y });
-    
+
     if (this.#hide === true) {
       this.data.hide = true;
     } else {
       delete this.data.hide;
     }
-    
+
     return this;
   }
-  
+
   async wait(time = 0) {
     await sleep(time);
     return this;
   }
-  
+
   toggle(attributeMap, { time = 25, retries = 0 }) {
     if (this.isBusy) {
       this.#updateQueue.push(() => {
         this.toggle.bind(this)(attributeMap, { time, retries })
       })
-      
+
       return this;
     }
-    
+
     this.isBusy = true;
-    
+
     const model = { ...this.#model }
     this.update(attributeMap)
-    
+
     setTimeout(() => {
       this.update(model)
       this.isBusy = false;
     }, time);
-    
+
     return this;
   }
-  
+
   activate() {
     this.show();
     this.toggleEvents(true);
-    
+
     return this;
   }
-  
+
   deactivate() {
     this.hide();
     this.toggleEvents(false);
-    
+
     return this;
   }
-  
-  
+
+
   toggleEvents(v) {
     this.data.pointerEvents = v ?? !this.data.pointerEvents;
     return this;
   }
-  
+
   appendDOM(obj) {
     this.dom.append(obj.dom)
     return this;
   }
-  
+
   #normalizeSpatialPatch(source = {}) {
     const basePoint = this.#model.point ?? new CanvasPoint(this.#model.x ?? 0, this.#model.y ?? 0);
     const pointSource = source.point ?? {
@@ -290,7 +289,7 @@ export class CanvasObject extends EventEmitter {
       y: source.y ?? basePoint.y,
     };
     const point = CanvasPoint.from(pointSource);
-    
+
     return {
       ...source,
       point,
@@ -298,8 +297,8 @@ export class CanvasObject extends EventEmitter {
       y: point.y,
     };
   }
-  
+
   getEl(selector = '') { return this.dom.querySelector(selector); }
-  
+
   getEls(selector = '') { return [...this.dom.querySelectorAll(selector)]; }
 }
--- run-canvas.js
+++ run-canvas.js
@@ -31,15 +31,15 @@ const { map, scan, tap, filter, bufferTime, timestamp } = operators;
 
 const useTemplate = (templateName, options = {}) => {
 	const el = document.querySelector(`[data-template="${templateName}"]`).cloneNode(true);
-	
+
 	delete el.dataset.template;
-	
+
 	if (options.dataset) Object.assign(el.dataset, options.dataset);
-	
+
 	if (options.id) el.id = options.id;
-	
+
 	if (options.fill) el.style.fill = options.fill;
-	
+
 	return el;
 };
 
@@ -53,21 +53,21 @@ const computeArrowEndpoint = (origin, tileCenter, tileSize = [1, 1]) => {
 	const [ox, oy] = origin;
 	const [tx, ty] = tileCenter;
 	const [tw, th] = tileSize;
-	
+
 	const dx = tx - ox;
 	const dy = ty - oy;
 	const dist = Math.hypot(dx, dy);
 	const ux = dx / dist;
 	const uy = dy / dist;
-	
+
 	const offset = Math.min(
 		(tw / 2) / Math.abs(ux),
 		(th / 2) / Math.abs(uy)
 	);
-	
+
 	const ex = tx - ux * offset;
 	const ey = ty - uy * offset;
-	
+
 	return [ex, ey];
 };
 
@@ -79,14 +79,14 @@ const createEdgeLine = (pt1, pt2) => {
 		[pt1.x + 0.5, pt1.y + 0.5],
 		[pt2.x + 0.5, pt2.y + 0.5]
 	);
-	
+
 	lineEl.setAttribute('x1', pt1.x + 0.5);
 	lineEl.setAttribute('y1', pt1.y + 0.5);
 	lineEl.setAttribute('x2', endX);
 	lineEl.setAttribute('y2', endY);
 	lineHandle.setAttribute('cx', endX);
 	lineHandle.setAttribute('cy', endY);
-	
+
 	return line;
 };
 
@@ -104,18 +104,18 @@ export const runCanvas = async (mapId) => {
 	let contextMenu;
 	let selectMapById;
 	const subscriptions = new Map();
-	
+
 	// const loopEngine = new LoopEngine({
 	//   audioContext: audioEngine.ctx,
 	// });
-	
-	
+
+
 	const appStore = useAppState();
 	const { isRunning, setCurrentNode } = appStore
-	
+
 	let mapStore = useMapStore();
 	mapId = mapId && mapId.value ? mapId.value : mapId;
-	
+
 	// if (mapId) {
 	// 	await mapStore.setCurrentMapById(mapId)
 	// }
@@ -129,16 +129,16 @@ export const runCanvas = async (mapId) => {
 			],
 		}
 	})
-	
+
 	sceneModel = runtime.scene;
 	const loopEngine = runtime.loopEngine;
 	entityCollection = sceneModel.getColl(ModelTypes.ENTITIES);
 	graphModel = sceneModel.getColl(ModelTypes.GRAPH);
-	
+
 	// TODO - Move into scene/init
 	// graphModel = getGraphModel({ loopEngine, registry: ModelRegistry });
 	// entityCollection = new EntityCollection({ loopEngine, registry: ModelRegistry });
-	
+
 	// will be init'd in run time when that happens
 	// sceneModel = new SceneModel({
 	//   registry: ModelRegistry,
@@ -148,37 +148,37 @@ export const runCanvas = async (mapId) => {
 	//     { name: ModelTypes.ENTITIES, },
 	//   ],
 	// });
-	
-	
+
+
 	canvasEl = document.querySelector('#canvas');
 	svgCanvas = new SVGCanvas(canvasEl);
 	sceneObj = svgCanvas.scene;
 	tileLayer = sceneObj.getLayer('tile');
 	objectLayer = sceneObj.getLayer('object');
 	selectionBox = getTileSelector(objectLayer.dom);
-	
+
 	let isSelectingLinkTile = false;
 	let selectedTileBeingLinked = null;
-	
+
 	let selectedRange = [];
 	let getSelectedRange = () => tileLayer.findAll({ selected: true });
-	
+
 	const tileAt = (x, y) => tileLayer.getTileAt(x, y);
-	
+
 	const deselectRange = () => {
 		getSelectedRange().forEach((t, i) => {
 			t.update({ selected: false });
 		});
 	};
-	
+
 	contextMenu = contextMenu ?? new ContextMenu(svgCanvas);
 	contextMenu.disableItem('copy');
 	objectLayer.add(contextMenu ?? new ContextMenu(svgCanvas)).dom;
-	
+
 	if (entityCollection.has('actor1')) {
 		entityCollection.remove('actor1');
 	}
-	
+
 	subscriptions.set(
 		'entityCreate',
 		entityCollection.out({ type: 'entity:create' }).subscribe(e => {
@@ -197,13 +197,13 @@ export const runCanvas = async (mapId) => {
 				//   { type: 'scale', values: [1, 1], position: 2 },
 				// ],
 			});
-			
+
 			if (e.id === 'actor1') {
 				actor1 = canvasEntity;
 			}
 		})
 	);
-	
+
 	entityCollection.createActor({
 		type: 'actor',
 		id: 'actor1',
@@ -213,7 +213,7 @@ export const runCanvas = async (mapId) => {
 			point: { x: 0, y: 0 },
 		},
 	});
-	
+
 	entityCollection.createDarkSun({
 		type: 'dark-sun',
 		id: 'darksun1',
@@ -223,28 +223,28 @@ export const runCanvas = async (mapId) => {
 			point: { x: 10, y: 21 },
 		},
 	});
-	
+
 	selectMapById = selectMapById ?? await initMapControls(graphModel, svgCanvas);
-	
+
 	svgCanvas.setCanvasDimensions({ width: innerWidth, height: innerHeight });
-	
+
 	//! start binding
 	subscriptions.set(
 		'mapLoad',
 		graphModel.out({ type: 'map:load' }).subscribe(e => {
 			const { width, height, nodes, startNode } = e.data;
-			
+
 			selectionBox.setBounds({
 				minX: 0,
 				minY: 0,
 				maxX: graphModel.width,
 				maxY: graphModel.height
 			});
-			
+
 			svgCanvas.scene.getLayer('tile').loadTileSet({ width, height, nodes, startNode });
-			
+
 			const actor1Model = entityCollection.get('actor1');
-			
+
 			actor1Model.resetTraversal(startNode?.point);
 			actor1Model.update({
 				point: startNode.point,
@@ -255,11 +255,11 @@ export const runCanvas = async (mapId) => {
 			entityCollection.entities.forEach((entity) => {
 				sceneModel.resolver.syncEntityPosition(entity.id, entity.point);
 			});
-			
+
 			svgCanvas.layers.surface.setAttribute('transform', `translate(${Math.floor((graphModel.width + 2) / 2) - 0.3}, ${Math.floor((graphModel.height + 2) / 2) - 0.25})`);
 			svgCanvas.layers.surface.querySelector('#surface-map-name').setAttribute('transform', `translate(0, ${-((graphModel.height / 2)) - 3}) scale(0.4)`);
 		}));
-	
+
 	subscriptions.set(
 		'nodeUpdates',
 		graphModel.out({ type: 'node:update' }).pipe(
@@ -269,7 +269,7 @@ export const runCanvas = async (mapId) => {
 			})
 		).subscribe()
 	);
-	
+
 	subscriptions.set(
 		'actorRender',
 		entityCollection.out({
@@ -283,30 +283,30 @@ export const runCanvas = async (mapId) => {
 			const actor1Model = entityCollection.get('actor1');
 			if (event.type === 'actor:update') {
 				if (!actor1) return;
-				
+
 				const currRotate = actor1.transforms.rotation;
-				
+
 				actor1.update({
 					...event.data,
 					x: event.data?.point?.x ?? event.data?.x,
 					y: event.data?.point?.y ?? event.data?.y,
 				});
-				
+
 				return;
 			}
-			
+
 			if (event.type === 'actor:move') {
 				if (!actor1) return;
 				actor1.update({ x: event.point.x, y: event.point.y });
 				return;
 			}
-			
+
 			if (event.type === 'actor:teleport') {
 				if (!actor1) return;
 				actor1.update({ x: event.point.x, y: event.point.y, teleporting: true });
 				return;
 			}
-			
+
 			if (['actor:stop', 'actor:idle', 'actor:goal', 'actor:map-link'].includes(event.type)) {
 				if (!actor1) return;
 				const point = event.point ?? actor1Model.currentPoint;
@@ -314,19 +314,19 @@ export const runCanvas = async (mapId) => {
 			}
 		})
 	);
-	
+
 	const darkSunNotes = [
 		{ frequency: 246.94, velocity: 0.175 },
 		{ frequency: 329.25, velocity: 0.125 },
 		{ frequency: 234.96, velocity: 0.15 },
 	]
-	
+
 	const cMajorTriad = [
 		{ frequency: 261.63, velocity: 0.175 },
 		{ frequency: 329.63, velocity: 0.125 },
 		{ frequency: 392, velocity: 0.15 },
 	]
-	
+
 	subscriptions.set(
 		'darksunTravel',
 		entityCollection.out({
@@ -335,19 +335,19 @@ export const runCanvas = async (mapId) => {
 		}).subscribe(async ({ id, point, goalPoint }) => {
 			const entity = entityCollection.get(id);
 			const curr = graphModel.getNodeAtPoint(point);
-			
-			
+
+
 			for (let { frequency, velocity } of darkSunNotes) {
 				const index = darkSunNotes.findIndex(_ => _.frequency === frequency)
 				const mod = index * 3
 				const delay = mod * 25
-				
+
 				await sleep(delay);
-				
+
 				audioNote1(curr, { forceNewNote: true, frequency, velocity });
 				playChord
 			}
-			
+
 			// audioNote1(curr, { forceNewNote: true, frequency: 300, velocity: 0.15 });
 			// await sleep(75);
 			// audioNote1(curr, { forceNewNote: true, frequency: 400, velocity: 0.15 });
@@ -355,9 +355,9 @@ export const runCanvas = async (mapId) => {
 			// audioNote1(curr, { forceNewNote: true, frequency: 490, velocity: 0.15 });
 		})
 	);
-	
+
 	let neighborIndex = 0;
-	
+
 	subscriptions.set(
 		'actorTravel',
 		entityCollection.out({
@@ -366,22 +366,22 @@ export const runCanvas = async (mapId) => {
 		}).subscribe(async ({ point, goalPoint }) => {
 			const curr = graphModel.getNodeAtPoint(point);
 			const goal = graphModel.getNodeAtPoint(goalPoint);
-			
+
 			// graphModel.findAny({ active: true })
 			// 	.forEach(_ => _.active === false)
 			graphModel.findNodes(n => n.active === true)
 				.forEach(_ => _.update({ active: false }));
-			
+
 			goal.update({ active: true });
-			
+
 			// audioNote1(curr, { forceNewNote: true, frequency: cMajorTriad[0].frequency, velocity: 0.3 });
 			// await sleep(50);
-			
+
 			// audioNote1(curr, { forceNewNote: true, frequency: cMajorTriad[1].frequency, velocity: 0.2 });
 			// await sleep(85);
 			// audioNote1(curr, { forceNewNote: true, frequency: cMajorTriad[2].frequency, velocity: 0.2 });
 			await sleep(50);
-			
+
 			playChord({ point: curr.point, })
 			// audioNote1(curr, { forceNewNote: true });
 		})
@@ -452,7 +452,7 @@ export const runCanvas = async (mapId) => {
 				filter: ({ id }) => entityCollection.get(id).type === 'dark-sun',
 			}).subscribe(async ({ id, point }) => {
 				const node = graphModel.getNodeAtPoint(point);
-				
+
 				objectLayer.get(id)?.update({ point: node.point });
 			})
 		);
@@ -461,32 +461,32 @@ export const runCanvas = async (mapId) => {
 	// 	'collision',
 	// 	sceneModel.out({ type: 'collision' })
 	// 	.subscribe(async (event) => {
-	// 		console.warn('event', event)			
+	// 		console.warn('event', event)
 	// 		const { id, entering, actors } = event
-	
+
 	// 		const { newObjectId, objectIds } = data;
 	// 		const newOccupant = entityCollection.get(newObjectId);
 	// 		const node = tileLayer.get(id);
-	
+
 	// 		if (!newOccupant || newOccupant.type !== 'dark-sun') {
 	// 			return;
 	// 		}
-	
+
 	// 		newOccupant?.reverseCourse?.();
-	
+
 	// 		const dso = objectLayer.get(newObjectId);
 	// 		dso.toggle({ recoiling: true }, { time: 200 });
-	
+
 	// 		node.toggle({ recoiling: true }, { time: 500 });
-	
+
 	// 		objectIds.forEach(async (id, i) => {
 	// 			const o = entityCollection.get(id);
-	
+
 	// 			if (o === newOccupant) {
 	// 				return
 	// 			}
 	// 			// o.setGoalPoint(node.point);
-	
+
 	// 			if (o.travelTo) {
 	// 				await sleep(180)
 	// 				o.travelTo(node.point);
@@ -498,23 +498,23 @@ export const runCanvas = async (mapId) => {
 	// 			}
 	// 		})
 	// 		await sleep(50)
-	
+
 	// 		audioNote1(null, {
 	// 			forceNewNote: true,
 	// 			frequency: 220,
 	// 			velocity: 0.3,
 	// 		});
-	
+
 	// 		await sleep(25);
-	
+
 	// 		audioNote1(null, {
 	// 			forceNewNote: true,
 	// 			frequency: 275,
 	// 			velocity: 0.15,
 	// 		});
-	
+
 	// 		await sleep(50);
-	
+
 	// 		audioNote1(null, {
 	// 			forceNewNote: true,
 	// 			frequency: 325,
@@ -527,40 +527,40 @@ export const runCanvas = async (mapId) => {
 		sceneModel.out({ type: 'interaction:collision' })
 		.subscribe(async (event) => {
 			const { point, entering } = event;
-			
+
 			const newOccupant = entityCollection.get(entering);
 			const node = tileLayer.get(graphModel.pointToAddress(point));
-			
+
 			if (!newOccupant || newOccupant.type !== 'dark-sun' || !node) {
 				return;
 			}
-			
+
 			const dso = objectLayer.get(entering);
 			if (!dso) {
 				return;
 			}
-			dso.toggle({ recoiling: true }, { time: 200 });
-			
+			dso.toggle({ point, recoiling: true }, { time: 150 });
+
 			node.toggle({ recoiling: true }, { time: 500 });
-			
+
 			await sleep(50);
-			
+
 			audioNote1(null, {
 				forceNewNote: true,
 				frequency: 220,
 				velocity: 0.3,
 			});
-			
+
 			await sleep(25);
-			
+
 			audioNote1(null, {
 				forceNewNote: true,
 				frequency: 275,
 				velocity: 0.15,
 			});
-			
+
 			await sleep(50);
-			
+
 			audioNote1(null, {
 				forceNewNote: true,
 				frequency: 325,
@@ -570,165 +570,165 @@ export const runCanvas = async (mapId) => {
 	);
 	const unwatchCurrentMap = watch(mapStore.currentMap, (newMap, oldMap) => {
 		if (!newMap.id) return;
-		
+
 		const mapData = toValue(newMap);
-		
+
 		graphModel.fromMap(mapData);
 	}, { immediate: true });
-	
+
 	const unwatchIsRunning = watch(isRunning, (newVal, oldVal) => {
 		if (newVal === true && oldVal === false) {
 			loopEngine.start()
 		}
-		
+
 		if (newVal === false && oldVal === true) {
 			loopEngine.pause()
 		}
 	}, { immediate: true });
-	
+
 	const unsubscribeSelectionBox = selectionBox.on('selection', ({ type, points, ...range }) => {
 		const { start, end } = range;
-		
+
 		selectedRange = graphModel.getRange({ type, points, ...range });
-		
+
 		contextMenu.update({ x: start.x, y: start.y - 2 }).show();
 	});
-	
-	
+
+
 	//! end 1bindings
-	
+
 	// loopEngine.start();
-	
+
 	const blurContextMenu = (e) => {
 		const edgeLines = [...objectLayer.dom.querySelectorAll('.edge-line')];
-		
+
 		edgeLines.forEach(el => {
 			el.remove();
 		});
-		
+
 		if (contextMenu.isVisible) {
 			selectionBox.remove();
-			
+
 			contextMenu.hide();
 			contextMenu.toggleActions(false);
 		}
 	};
-	
+
 	const handleTileClick = async ({ type, detail }) => {
-		
+
 		if (!isRunning.value) return;
 		if (contextMenu.isVisible) {
 			blurContextMenu();
 			return;
 		};
-		
+
 		if (isSelectingLinkTile === true) return;
-		
+
 		if (!type || type !== 'tile:click') {
 			console.warn('NON TILE CLICK, RETURNING FROM LOOP', type, detail);
 			return;
 		}
-		
+
 		const prevGoal = graphModel.findNode(n => n.current === true);
-		
+
 		if (prevGoal) {
 			// prevGoal.update({ active: false })
 		}
-		
+
 		const goalNode = graphModel.getNodeByAddress(detail.id);
-		
+
 		if (!goalNode || !goalNode.isTraversable) {
 			console.warn('NO GOAL OR GOAL NOT TRAVERSABLE. Early return');
 			console.warn(goalNode?.id, goalNode?.isTraversable);
 			entityCollection.get('actor1').stop();
-			
+
 			return;
 		}
-		
+
 		entityCollection.get('actor1').travelTo(goalNode.point);
 	};
-	
-	
+
+
 	const handleEditTileClick = async (targetNode) => {
 		if (!targetNode) {
 			console.warn('handleEditTileClick !targetnode', );
 			blurContextMenu();
 			return;
 		}
-		
+
 		const { tileType, target, selected } = targetNode;
-		
+
 		if (tileType === 'teleport') {
 			if (target) {
 				// TODO: Make lines into Canvas Object
 				const line = createEdgeLine(targetNode, target);
-				
+
 				objectLayer.dom.append(line);
-				
+
 				line.addEventListener('pointermove', e => {
 					e.stopPropagation();
 					e.preventDefault();
 					console.warn('line');
 					const newPoint = domPoint(svgCanvas.scene.dom, e.clientX, e.clientY);
 					selectedTileBeingLinked = targetNode;
-					
+
 					line.firstElementChild.setAttribute('x2', Math.floor(newPoint.x) + 0.5);
 					line.firstElementChild.setAttribute('y2', Math.floor(newPoint.y) + 0.5);
 				});
-				
+
 				line.addEventListener('pointerup', e => {
 					e.stopPropagation();
 					e.preventDefault();
-					
-					
+
+
 					const newPoint = domPoint(line.parentElement, e.clientX, e.clientY);
 					const node = graphModel.getNodeAtPoint({ x: Math.floor(newPoint.x), y: Math.floor(newPoint.y) });
 					line.firstElementChild.setAttribute('x2', node.x + 0.5);
 					line.firstElementChild.setAttribute('y2', node.y + 0.5);
-					
+
 					handleTileLinkSelect({ node });
 					blurContextMenu();
 				});
 			}
-			
+
 			contextMenu.show();
 			contextMenu.toggleActions(true);
 		} else {
 			contextMenu.toggleActions(false);
 		}
-		
+
 		targetNode.update({ selected: true });
-		
+
 		selectionBox.insertAt({ x: targetNode.x, y: targetNode.y });
 	};
-	
+
 	const handleTileLinkSelect = (e) => {
 		const nodeToLink = e.node ?? graphModel.getNodeAtPoint({ ...e.detail });
-		
+
 		const node = selectedTileBeingLinked;
-		
+
 		if (nodeToLink.tileType !== 'teleport') {
 			nodeToLink.update({ tileType: 'teleport', target: { x: node.x, y: node.y } });
 		}
-		
+
 		node.update({ target: { x: nodeToLink.x, y: nodeToLink.y } });
-		
+
 		isSelectingLinkTile = false;
 		svgCanvas.layers.tile.dataset.isSelectingLinkTile = false;
 		selectedTileBeingLinked = null;
-		
+
 		return;
 	};
-	
+
 	svgCanvas.addEventListener('surface:click', (e) => {
 		entityCollection.get('actor1').stop();
 		blurContextMenu();
 	});
-	
+
 	svgCanvas.addEventListener('tile:click', (e) => {
 		e.preventDefault();
 		e.stopPropagation();
-		
+
 		const targetNode = graphModel.getNodeByAddress(e.detail.id);
 		if (isSelectingLinkTile) {
 			// handleTileLinkSelect(e);
@@ -739,31 +739,31 @@ export const runCanvas = async (mapId) => {
 			handleEditTileClick(targetNode);
 		}
 	});
-	
+
 	svgCanvas.addEventListener('tile:contextmenu', (e) => {
 		const targetNode = graphModel.getNodeByAddress(e.detail.id);
 		console.warn('CONTEXT MENU')
 		handleEditTileClick(targetNode);
 	});
-	
+
 	contextMenu.on('tile-action', data => {
 		const selectedOptionValue = data.type;
 		const selectedTileTypeName = data.type;
 		const selectedNode = selectedRange[0];
-		
+
 		if (!selectedNode) return;
-		
+
 		if (selectedOptionValue === 'copy') {
 			sourceRange = selectedRange;
 		}
-		
+
 		if (selectedOptionValue === 'link-teleport') {
-			
+
 			isSelectingLinkTile = true;
 			svgCanvas.layers.tile.dataset.isSelectingLinkTile = true;
-			
+
 			selectedTileBeingLinked = selectedNode;
-			
+
 			return;
 		}
 		else {
@@ -775,8 +775,8 @@ export const runCanvas = async (mapId) => {
 			});
 		};
 	});
-	
-	
+
+
 	return () => {
 		subscriptions.forEach((sub, k) => {
 			console.warn('unsubscribing: ', k)
